7d2cee908b01849f85abf9fd4d4ad1c100f57140
niko <70974700+niboko@users.noreply.github.com>
2022-01-19 17:58:05 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 DIR=".cool/source/alhambra"
 
 ALIAS='alias alhambra="source ~/.cool/source/alhambra/alhambra.sh"'
+ALIAS_FIX='alias alhambra-fix="source ~/.cool/source/alhambra/alhambra-fix.sh"'
 
 cd 
 
@@ -17,15 +18,31 @@ fi
 
 
 cd $DIR && curl  -O  "https://raw.githubusercontent.com/niboko/alhambra/main/alhambra.sh" 
-echo "file downloaded"
+cd
+cd $DIR && curl  -O  "https://raw.githubusercontent.com/niboko/alhambra/main/alhambra-fix.sh" 
+echo "files downloaded"
 
 cd
 
-echo $ALIAS >> .zshrc
+if ! grep -Fxq 'alias alhambra="source ~/.cool/source/alhambra/alhambra.sh"' ".zshrc" 
+then
+    echo $ALIAS >> .zshrc
+else
+	echo "ALIAS ALHAMBRA EXISTS"
+fi
+
+if ! grep -Fxq 'alias alhambra-fix="source ~/.cool/source/alhambra/alhambra-fix.sh"' ".zshrc" 
+then
+    echo $ALIAS_FIX >> .zshrc
+else
+	echo "ALIAS ALHAMBRA_FIX EXISTS"
+fi
+
+
 echo ""
 
 echo "alhambra installed, please restart your terminal."
-echo "now you can use 'alhambra' to update your system"
+echo "now you can use 'alhambra' to update your system and 'alhambra-fix' to have a higher likelyhood of avoiding issues..."
 
 
 }
